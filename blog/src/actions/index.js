563a9564ede49f8432c6d4cb4d9528f47cba65eb
timlee0119 <b05705006@ntu.edu.tw>
2020-04-24 14:15:31 +0800

    update

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,31 @@
-import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async () => {
-    const response = await jsonPlaceholder.get("/posts");
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
 
-    return {
-        type: "FETCH_POST",
-        payload: response
-    };
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
 };
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
